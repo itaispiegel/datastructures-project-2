5e419821a5196d3b28cb18e089f28a40dfd60392
OrenShacham <orenshacham@mail.tau.ac.il>
2022-05-17 12:57:42 +0300

    Implemented Find, Insert and Delete and added the DeletedHashTableElement

--- src/main/java/il/ac/tau/cs/ds/OAHashTable.java
+++ src/main/java/il/ac/tau/cs/ds/OAHashTable.java
@@ -2,6 +2,12 @@ package il.ac.tau.cs.ds;
 
 public abstract class OAHashTable implements IHashTable {
 
+    public static class DeletedHashTableElement extends HashTableElement {
+        public DeletedHashTableElement(long key, long value) {
+            super(key, value);
+        }
+    }
+
     private HashTableElement[] table;
 
     public OAHashTable(int m) {
@@ -12,18 +18,52 @@ public abstract class OAHashTable implements IHashTable {
 
     @Override
     public HashTableElement Find(long key) {
-        // TODO implement find
+        for (int i = 0; i < table.length; i++) {
+            int index = Hash(key, i);
+            if (table[index] == null) {
+                return null;
+            } else if (table[index].GetKey() == key) {
+                return table[index];
+            }
+        }
         return null;
     }
 
     @Override
     public void Insert(HashTableElement hte) throws TableIsFullException, KeyAlreadyExistsException {
-        // TODO implement insertion
+        int firstAvailableIndex = -1;
+
+        for (int i = 0; i < table.length; i++) {
+            int index = Hash(hte.GetKey(), i);
+            if (table[index] == null) {
+                firstAvailableIndex = index;
+                break;
+            } else if (table[index] instanceof DeletedHashTableElement && firstAvailableIndex == -1) {
+                firstAvailableIndex = index;
+            } else if (table[index].GetKey() == hte.GetKey()) {
+                throw new KeyAlreadyExistsException(hte);
+            }
+        }
+
+        if (firstAvailableIndex > -1) {
+            table[firstAvailableIndex] = hte;
+        } else {
+            throw new TableIsFullException(hte);
+        }
     }
 
     @Override
     public void Delete(long key) throws KeyDoesntExistException {
-        // TODO implement deletion
+        for (int i = 0; i < table.length; i++) {
+            int index = Hash(key, i);
+            if (table[index] == null) {
+                throw new KeyDoesntExistException(key);
+            } else if (table[index].GetKey() == key) {
+                table[index] = new DeletedHashTableElement(key, 0);
+                return;
+            }
+        }
+        throw new KeyDoesntExistException(key);
     }
 
     /**
