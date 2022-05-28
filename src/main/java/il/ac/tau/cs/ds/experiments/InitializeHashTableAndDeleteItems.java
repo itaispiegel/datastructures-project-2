bfc1893f8cb322f2c8242a169fd4a151b8f66755
itaispiegel <itai.spiegel@gmail.com>
2022-05-28 11:22:57 +0300

    Fix InitializeHashTableAndDeleteItems experiment

--- src/main/java/il/ac/tau/cs/ds/experiments/InitializeHashTableAndDeleteItems.java
+++ src/main/java/il/ac/tau/cs/ds/experiments/InitializeHashTableAndDeleteItems.java
@@ -4,6 +4,7 @@ import il.ac.tau.cs.ds.table.DoubleHashTable;
 import il.ac.tau.cs.ds.table.HashTableElement;
 import il.ac.tau.cs.ds.table.IHashTable;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Random;
 
@@ -11,39 +12,46 @@ public class InitializeHashTableAndDeleteItems {
     private static final int m = 10000019;
     private static final long p = 1000000007L;
 
+    private static long insertAndDeleteItemsAndMeasureTime(DoubleHashTable table,
+                                                           Collection<HashTableElement> sequence) throws
+            IHashTable.TableIsFullException, IHashTable.KeyAlreadyExistsException, IHashTable.KeyDoesntExistException {
+
+        long startTimeMs = System.currentTimeMillis();
+        for (HashTableElement element : sequence) {
+            table.Insert(element);
+        }
+
+        for (HashTableElement element : sequence) {
+            table.Delete(element.GetKey());
+        }
+        long endTimeMs = System.currentTimeMillis();
+        return endTimeMs - startTimeMs;
+    }
+
     public static void main(String[] args) throws
             IHashTable.TableIsFullException, IHashTable.KeyAlreadyExistsException, IHashTable.KeyDoesntExistException {
 
         Random random = new Random(10);
         DoubleHashTable table = new DoubleHashTable(m, p);
 
-        List<HashTableElement> sequence = ExperimentUtils.generateRandomSequence(m / 2, random);
-        int sequenceSize = sequence.size();
+        long totalTime = 0;
+        for (int i = 1; i < 4; i++) {
+            List<HashTableElement> sequence = ExperimentUtils.generateRandomSequence(m / 2, random);
 
-        for (int i = 0; i < sequenceSize; i++) {
-            HashTableElement hte = sequence.get(i);
-            long startTime = System.nanoTime();
-            table.Insert(hte);
-            long endTime = System.nanoTime();
-
-            if (i < 3) {
-                System.out.println("Running time for inserting at iteration #" + (i + 1) + ": " +
-                        (endTime - startTime) + "ns");
-            }
+            long elapsedTimeMs = insertAndDeleteItemsAndMeasureTime(table, sequence);
+            System.out.println(i + " took " + elapsedTimeMs + "ms");
+            totalTime += elapsedTimeMs;
         }
+        System.out.println("First 3 iterations: " + totalTime + "ms");
 
-        System.out.println("Inserted all items, now starting to delete\n");
+        totalTime = 0;
+        for (int i = 4; i < 7; i++) {
+            List<HashTableElement> sequence = ExperimentUtils.generateRandomSequence(m / 2, random);
 
-        for (int i = 0; i < sequenceSize; i++) {
-            HashTableElement hte = sequence.get(i);
-            long startTime = System.nanoTime();
-            table.Delete(hte.GetKey());
-            long endTime = System.nanoTime();
-
-            if (i < 3) {
-                System.out.println("Running time for deletion at iteration #" + (i + 1) + ": " +
-                        (endTime - startTime) + "ns");
-            }
+            long elapsedTimeMs = insertAndDeleteItemsAndMeasureTime(table, sequence);
+            System.out.println(i + " took " + elapsedTimeMs + "ms");
+            totalTime += elapsedTimeMs;
         }
+        System.out.println("Last 3 iterations: " + totalTime + "ms");
     }
 }
--- src/main/java/il/ac/tau/cs/ds/table/OAHashTable.java
+++ src/main/java/il/ac/tau/cs/ds/table/OAHashTable.java
@@ -3,8 +3,8 @@ package il.ac.tau.cs.ds.table;
 public abstract class OAHashTable implements IHashTable {
 
     public static class DeletedHashTableElement extends HashTableElement {
-        public DeletedHashTableElement(long key, long value) {
-            super(key, value);
+        public DeletedHashTableElement(long key) {
+            super(key, 0);
         }
     }
 
@@ -28,29 +28,31 @@ public abstract class OAHashTable implements IHashTable {
 
     @Override
     public HashTableElement Find(long key) {
-        for (int i = 0; i < table.length; i++) {
-            int index = Hash(key, i);
-            if (table[index] == null) {
-                return null;
-            } else if (table[index].GetKey() == key) {
-                return table[index];
-            }
+        try {
+            int index = getIndexInTable(key);
+            return table[index];
+        } catch (KeyDoesntExistException e) {
+            return null;
         }
-        return null;
     }
 
     @Override
     public void Insert(HashTableElement hte) throws TableIsFullException, KeyAlreadyExistsException {
+        long key = hte.GetKey();
         int firstAvailableIndex = -1;
 
         for (int i = 0; i < table.length; i++) {
-            int index = Hash(hte.GetKey(), i);
+            int index = Hash(key, i);
             if (table[index] == null) {
-                firstAvailableIndex = index;
+                firstAvailableIndex = firstAvailableIndex == -1 ? index : firstAvailableIndex;
                 break;
-            } else if (table[index] instanceof DeletedHashTableElement && firstAvailableIndex == -1) {
-                firstAvailableIndex = index;
-            } else if (table[index].GetKey() == hte.GetKey()) {
+            }
+
+            if (table[index] instanceof DeletedHashTableElement) {
+                if (firstAvailableIndex == -1) {
+                    firstAvailableIndex = index;
+                }
+            } else if (table[index].GetKey() == key) {
                 throw new KeyAlreadyExistsException(hte);
             }
         }
@@ -65,16 +67,22 @@ public abstract class OAHashTable implements IHashTable {
 
     @Override
     public void Delete(long key) throws KeyDoesntExistException {
+        int index = getIndexInTable(key);
+        table[index] = new DeletedHashTableElement(key);
+        size--;
+    }
+
+    private int getIndexInTable(long key) throws KeyDoesntExistException {
         for (int i = 0; i < table.length; i++) {
             int index = Hash(key, i);
-            if (table[index] == null) {
-                throw new KeyDoesntExistException(key);
-            } else if (table[index].GetKey() == key) {
-                table[index] = new DeletedHashTableElement(key, 0);
-                size--;
-                return;
+            HashTableElement hte = table[index];
+            if (hte == null) {
+                break;
+            } else if (hte.GetKey() == key) {
+                return index;
             }
         }
+
         throw new KeyDoesntExistException(key);
     }
 
